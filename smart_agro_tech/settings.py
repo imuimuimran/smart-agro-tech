4d39212c4a6935b73a570e0bdd634e94b941b08b
Mohd Imran Hossain <hossainmohdimran@gmail.com>
2025-06-18 13:52:15 +0600

    updated with SQLite

--- smart_agro_tech/settings.py
+++ smart_agro_tech/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,19 +81,15 @@ WSGI_APPLICATION = 'smart_agro_tech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if os.environ.get('DATABASE_URL'):
-    # Use PostgreSQL on Render
-    DATABASES = {
-        'default': dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=600)
+
+
+    
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    # Use SQLite locally
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
